684cb492de971872f53ec84bc450da404bc6c06e
Hyun-Woo Lee <54518925+l2hyunwoo@users.noreply.github.com>
2023-06-05 11:46:54 +0900

    [feature/config-storybook] Configure storybook (#3)
    * configure storybook dependencies
    
    * theme config
    
    * storybook configuration

---
+++ .storybook/main.ts
@@ -0,0 +1,39 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
+import * as path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-postcss",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  staticDirs: ["public"],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-private-methods",
+      "@babel/plugin-proposal-private-property-in-objects",
+    ],
+  }),
+  webpackFinal: (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, "../tsconfig.json")
+      })
+    ]
+
+    return config;
+  }
+};
+export default config;
