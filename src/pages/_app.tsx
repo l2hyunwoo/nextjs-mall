2b148b3c68f8c81bb9dadec1f9781bec2d831aac
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-06-05 10:44:26 +0900

    Set GlobalStyle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,49 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createGlobalStyle } from "styled-components";
+import Head from "next/head";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
+    Helvetica Neue, sans-serif;
+  }
+  
+  * {
+    box-sizing: border-box;
+  }
+  
+  a {
+    cursor: pointer;
+    text-decoration: none;
+    transition: .25s;
+    color: #000;
+  }
+  
+  ol, ul {
+    list-style: none;
+  }
+`
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta key="charset" name="charset" content="utf-8" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, shrink-to-fit=no, maximum-scale=5.0"
+        />
+        <meta property="og:locale" content="ko_KR" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
